14bc8a1f1706459889e4a3ca2aa0cd2fb66e9edb
Necromunger <lil_tommy_tom@hotmail.com>
2026-07-21 12:47:54 +1000

    rename

--- PlantBasedLeather/PlantBasedLeatherModSystem.cs
+++ PlantBasedLeather/PlantBasedLeatherModSystem.cs
@@ -1,29 +1,6 @@
-﻿using Vintagestory.API.Client;
-using Vintagestory.API.Common;
-using Vintagestory.API.Config;
-using Vintagestory.API.Server;
+﻿using Vintagestory.API.Common;
 
 namespace PlantBasedLeather
 {
-    public class PlantBasedLeatherModSystem : ModSystem
-    {
-
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
-        public override void Start(ICoreAPI api)
-        {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
-        }
-
-        public override void StartServerSide(ICoreServerAPI api)
-        {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("plantbasedleather:hello"));
-        }
-
-        public override void StartClientSide(ICoreClientAPI api)
-        {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("plantbasedleather:hello"));
-        }
-
-    }
+    public class PlantBasedLeatherModSystem : ModSystem { }
 }
